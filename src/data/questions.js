1b09844d192d59402f59d06be52c025924836299
Aurelio Chagas <68388036+Aurelio-ch@users.noreply.github.com>
2022-11-30 16:48:58 -0300

    Add files via upload

---
+++ src/data/questions.js
@@ -0,0 +1,31 @@
+const data = [
+  {
+    question: "Qual é o principal ingrediente para preparar a Tiquira?",
+    options: [
+      "Batata doce",
+      "Mandioca",
+      "Beterraba",
+    ],
+    answer: "Mandioca",
+  },
+  {
+    question: "O doce de espécie é preparado para qual festejo popular maranhense?",
+    options: [
+      "Festa do Divino Espírito Santo",
+      "Festejo de São Benedito",
+      "São João",
+    ],
+    answer: "São João",
+  },
+  {
+    question: "O Cuxá é preparado com qual tipo de erva?",
+    options: [
+      "Erva cidreira",
+      "Capim Limão",
+      "Vinagreira",
+    ],
+    answer: "Vinagreira",
+  },
+];
+
+export default data;
